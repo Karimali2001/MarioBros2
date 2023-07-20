7658f416d1a14ba9a0b27993e63c46416de0a48c
karimsh2001 <karimsh2001@gmail.com>
2023-07-19 23:27:28 -0400

    comentar juego en menu

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -224,8 +224,8 @@ public class Menu extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        this.setVisible(false);
-        new Game();
+       // this.setVisible(false);
+        //new Game();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
